9d2087db6bbac673b7ad0651978c08b748d7a99e
zeynepesirgenci <46682524+zeynepesirgenci@users.noreply.github.com>
2022-11-21 17:39:24 +0300

    Initial commit

---
+++ src/useLocalStorage.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function getSavedValue(key, initialValue) {
+  const savedValue = JSON.parse(localStorage.getItem(key));
+  if (savedValue) return savedValue;
+
+  if (initialValue instanceof Function) return initialValue();
+  return initialValue;
+}
+
+export default function useLocalStorage(key, initialValue) {
+  const [value, setValue] = useState(() => {
+    return getSavedValue(key, initialValue);
+  });
+
+  return [value, setValue];
+}
